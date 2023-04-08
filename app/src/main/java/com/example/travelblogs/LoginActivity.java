c9744096e4fe81473e167c271962f91fd727e186
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-04-08 19:20:47 +0530

    Login indicator and start the main activity

--- app/src/main/java/com/example/travelblogs/LoginActivity.java
+++ app/src/main/java/com/example/travelblogs/LoginActivity.java
@@ -1,11 +1,14 @@
 package com.example.travelblogs;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ProgressBar;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,6 +19,8 @@ public class LoginActivity extends AppCompatActivity {
     private TextInputLayout textUsernameLayout;
     private TextInputLayout textPasswordInput;
     private Button loginButton;
+
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -38,6 +43,9 @@ public class LoginActivity extends AppCompatActivity {
         // for validation message cleanup
         textUsernameLayout.getEditText().addTextChangedListener(createTextWatcher(textUsernameLayout));
         textPasswordInput.getEditText().addTextChangedListener(createTextWatcher(textPasswordInput));
+
+        // Login indicator
+        progressBar = findViewById(R.id.progressBar);
     }
 
     // Validation username and password
@@ -53,6 +61,9 @@ public class LoginActivity extends AppCompatActivity {
         }
         else if(!username.equals("admin") || !password.equals("admin")){
             showErrorDialog();
+        }
+        else{
+            proformLogin();
         }
     }
 
@@ -76,12 +87,34 @@ public class LoginActivity extends AppCompatActivity {
         };
     }
 
+    // Error dialog box
     private void showErrorDialog() {
         new AlertDialog.Builder(this)
                 .setTitle("Login Failed")
                 .setMessage("Username or password is not correct. Please try again.")
                 .setPositiveButton("OK", (dialog, which) -> dialog.dismiss()).show();
 
+    }
+
+    //Loading indicator
+    private void proformLogin(){
+        textUsernameLayout.setEnabled(false);
+        textPasswordInput.setEnabled(false);
+        loginButton.setVisibility(View.INVISIBLE);
+        progressBar.setVisibility(View.VISIBLE);
+
+        // after clicking the login button we want delay of 2sec
+        Handler handler = new Handler();
+        handler.postDelayed(()->{
+            startMainActivity();
+            finish(); // to finish the login activity
+        },2000);
+    }
+
+    // to start the main activity
+    private void startMainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
 
