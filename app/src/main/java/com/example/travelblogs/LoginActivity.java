cdb9fec00774a1b9aa32e56b7ef5773a155bc3de
Vipul Chaudhary <vipschaudhary044@gmail.com>
2023-04-08 19:00:18 +0530

    Handling the form validation and show error box

--- app/src/main/java/com/example/travelblogs/LoginActivity.java
+++ app/src/main/java/com/example/travelblogs/LoginActivity.java
@@ -1,16 +1,87 @@
 package com.example.travelblogs;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputLayout;
+
 public class LoginActivity extends AppCompatActivity {
+    private TextInputLayout textUsernameLayout;
+    private TextInputLayout textPasswordInput;
+    private Button loginButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_login);
+
+        textUsernameLayout = findViewById(R.id.textUsernameLayout);
+        textPasswordInput = findViewById(R.id.textPasswordInput);
+        loginButton = findViewById(R.id.loginButton);
+
+//        loginButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View v){
+//                LoginActivity.this.onLoginClicked();
+//            }
+//        });
+        loginButton = findViewById(R.id.loginButton);
+        loginButton.setOnClickListener(v -> onLoginClicked());
+
+        // for validation message cleanup
+        textUsernameLayout.getEditText().addTextChangedListener(createTextWatcher(textUsernameLayout));
+        textPasswordInput.getEditText().addTextChangedListener(createTextWatcher(textPasswordInput));
+    }
+
+    // Validation username and password
+    public void onLoginClicked(){
+        String username = textUsernameLayout.getEditText().getText().toString();
+        String password = textPasswordInput.getEditText().getText().toString();
+
+        if(username.isEmpty()){
+            textUsernameLayout.setError("Username must not be empty");
+        }
+        else if(password.isEmpty()){
+            textPasswordInput.setError("Password must not be empty");
+        }
+        else if(!username.equals("admin") || !password.equals("admin")){
+            showErrorDialog();
+        }
+    }
+
+    // validation message cleanup
+    private TextWatcher createTextWatcher(TextInputLayout textPasswordInput){
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                  // not needed
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                textPasswordInput.setError(null);
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                // not needed
+            }
+        };
+    }
+
+    private void showErrorDialog() {
+        new AlertDialog.Builder(this)
+                .setTitle("Login Failed")
+                .setMessage("Username or password is not correct. Please try again.")
+                .setPositiveButton("OK", (dialog, which) -> dialog.dismiss()).show();
+
     }
 }
 
